c1337d48c9bc863c1dd63ef34bbe8a9278583866
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-06-16 20:23:57 +0300

    step 1.3

--- page_loader/script.py
+++ page_loader/script.py
@@ -1,15 +1,34 @@
-import requests, os
+import requests
+import os
+from urllib.parse import urlparse
 
 
-def take_name(url: str): -> str
+URL = 'https://ru.hexlet.io/courses'
 
 
-    
+def get_file_name(url: str):
+    unpacked_url = urlparse(url)
+    converted_char_to_list = list(unpacked_url.netloc + unpacked_url.path)
+    replaced_list = [item if item.isalpha() or item.isdigit() else '-' for item in converted_char_to_list]
+    return ''.join(replaced_list) + '.html'
 
 
+def get_folder_name(url: str):
+    unpacked_url = urlparse(url)
+    converted_char_to_list = list(unpacked_url.netloc + unpacked_url.path)
+    replaced_list = [item if item.isalpha() or item.isdigit() else '-' for item in converted_char_to_list]
+    return ''.join(replaced_list) + '_files'
 
-def_path = os.getcwd()
-url = 'https://ru.hexlet.io/courses'
-r = requests.get(url, allow_redirects=True)
 
-open('facebook.html', 'wb').write(r.content)
+def get_save_path(path=os.getcwd()): # по умолчанию используется текущая папка
+    return os.path.join(path, '')
+
+
+def donwload(url: str, path: str):
+    response = requests.get(url)
+    path_to_saved_file = path + get_file_name(url)
+    with open(path_to_saved_file, 'wb') as file: # wb - окрывает чтение/запись как бинарный
+        file.write(response.content)
+    return path_to_saved_file
+
+print(donwload(URL, get_save_path()))
